85d5b94b420c3c09568ceceda3c3c1164b8d8bf6
Dylan Jaegers <dylanjaegers23@gmail.com>
2026-05-23 11:33:07 -0500

    Fix auth paths after route group removal

--- src/proxy.js
+++ src/proxy.js
@@ -44,7 +44,7 @@ export async function proxy(request) {
   )
 
   if (isProtectedRoute && !user) {
-    return NextResponse.redirect(new URL('/login', request.url))
+    return NextResponse.redirect(new URL('/auth/login', request.url))
   }
 
   return supabaseResponse
